6f92958d66fd095345e09bf8e2141bd7d9cfe692
Binayak Sinha <binayaksinha2004@gmail.com>
2026-08-16 14:11:00 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    static void dfs(int i,int j,char[][]grid){
+        if(i>=0 && j>=0 && i<grid.length && j<grid[0].length){
+        if(grid[i][j]=='1'){
+            grid[i][j]='0';
+            dfs(i-1,j,grid);
+            dfs(i,j-1,grid);
+            dfs(i+1,j,grid);
+            dfs(i,j+1,grid);
+        }
+        }
+    }
+    public int numIslands(char[][] grid) {
+        int val=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    val+=1;
+                    dfs(i,j,grid);
+                }
+            }
+        }
+        return val;
+    }
+}
